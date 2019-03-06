11fd8507c7af1335377dcadad338419e317746f0
Jerome Guyon <gus.guyon@gmail.com>
2019-03-06 13:04:49 +0100

    Mise à jour de la structure HTML du formulaire pour enlever le superflu

--- src/view/frontend/signin.php
+++ src/view/frontend/signin.php
@@ -4,30 +4,27 @@
 <h1><?= $title ?></h1>
 
 <div class="container">
-	<div class="row">
-		<div class="col-sm-12">
-			<div class="log_form">
-				<form action="index.php?p=login" method="post">
-					<div class="form-group row">
-							<label for="pseudo">Votre pseudo :</label>
-						<div class="col-sm-4">
-							<input type="text" class="form-control" name="pseudo"/>
-						</div>
+	<div class="col-sm-12">
+		<div class="log_form">
+			<form action="index.php?p=login" method="post">
+				<div class="form-group row">
+						<label for="pseudo">Votre pseudo :</label>
+					<div class="col-sm-4">
+						<input type="text" class="form-control" name="pseudo"/>
 					</div>
-					<div class="form-group row">
-							<label for="pwd">Mot de passe :</label>
-						<div class="col-sm-4">
-							<input type="password" class="form-control" name="pwd"/>
-						</div>
+				</div>
+				<div class="form-group row">
+						<label for="pwd">Mot de passe :</label>
+					<div class="col-sm-4">
+						<input type="password" class="form-control" name="pwd"/>
 					</div>
-					<div class="form-check row">
-					    <input type="checkbox" class="form-check-input" id="remember">
-					    <label class="form-check-label" for="remember">Se souvenir de moi</label>
-					</div>
-					<button type="submit" class="btn btn-primary" value="Valider"/>Valider</button>
-					</div>
-				</form>
-			</div>
+				</div>
+				<div class="form-check row">
+				    <input type="checkbox" class="form-check-input" id="remember">
+				    <label class="form-check-label" for="remember">Se souvenir de moi</label>
+				</div>
+				<button type="submit" class="btn btn-primary" value="Valider"/>Valider</button>
+			</form>
 		</div>
 	</div>
 </div>
