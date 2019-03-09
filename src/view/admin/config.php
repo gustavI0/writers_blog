cf99e2a347bac4a92986162d568e3aef5922afb2
Jerome Guyon <gus.guyon@gmail.com>
2019-03-09 15:05:13 +0100

    Gestion des cookies pour connexion automatique (à tester)

--- src/view/admin/config.php
+++ src/view/admin/config.php
@@ -1,7 +1,19 @@
 <?php
+	if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    }
+
     if (isset ($_COOKIE['pseudo']) && isset ($_COOKIE['pwd'])) {
         $pseudo = $_COOKIE['pseudo'];
         $pwd = $_COOKIE['pwd'];
-        echo $_COOKIE['pwd'];
-        login($pseudo, $pwd);
-    } else {
+
+        $userManager = new \OpenClassrooms\Blog\Model\UserManager();
+        $result = $userManager->getUserCred($pseudo);
+        
+        if ($pwd === $result['pwd']) {
+                session_start();
+                $_SESSION['id'] = $result['id'];
+                $_SESSION['pseudo'] = $pseudo;
+        }
+    }
