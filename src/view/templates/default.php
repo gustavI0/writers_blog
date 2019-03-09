523eb475ca7342134455c51ef580ee09c47fee3a
Jerome Guyon <gus.guyon@gmail.com>
2019-03-09 15:11:05 +0100

    Suppression du head pour l'inclure dans un fichier distinct à insérer dans les templates et éviter la répétition du code

--- src/view/templates/default.php
+++ src/view/templates/default.php
@@ -3,29 +3,27 @@ if(!isset($_SESSION))
     { 
         session_start(); 
     } 
-?>
+    
+include ('view/templates/head.php');
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title><?= $title ?></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="public/css/style.css" rel="stylesheet" />
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
-        <link href="public/css/style.css" rel="stylesheet" />
-    </head>
+?>
         
     <body>
         <nav>
             <div>
                 <?php
                 if (isset($_SESSION['id']) AND isset($_SESSION['pseudo'])) {
-                    echo '<p>Bonjour ' . $_SESSION['pseudo'] . ' ! <br />
-                        <a href="index.php?p=admin">Accès au panneau d\'administration</a><br />
-                        <a href="index.php?p=disconnect">Se déconnecter</a>';
+                    echo '<div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Bonjour ' . $_SESSION['pseudo'] . ' ! 
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="index.php?p=admin">Accès au panneau d\'administration</a>
+                    <a class="dropdown-item" href="index.php?p=disconnect">Se déconnecter</a>
+                    </div>
+                    </div>';
                 } else {
-                    echo '<p><a href="index.php?p=signin">Connexion</a></p>';
+                    echo '<button type="button" class="btn btn-secondary" onclick="window.location.href=\'index.php?p=signin\'">Connexion</button>';
                 }
                 ?>
             </div>
@@ -36,5 +34,4 @@ if(!isset($_SESSION))
 	    	</div>
     	</main>
     </body>
-    <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=jjfxgpy7rm00ytowzec4htmu0ppmvjeolcmd7or3nttqhw07"></script>
-</html>
+<?php include ('view/templates/footer.php'); ?>
