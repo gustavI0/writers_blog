97f5abec4259e03025e103c1050831681b6a7305
Jerome Guyon <gus.guyon@gmail.com>
2019-03-09 15:03:49 +0100

    Ajouts de classes et changement de la mise en page pour gérer l'arrivée de l'éditeur de forme du texte

--- src/view/admin/addPost.php
+++ src/view/admin/addPost.php
@@ -2,7 +2,7 @@
  
 <?php ob_start(); ?>
 <h1>Le blog de l'écrivain</h1>
-<p><a href="index.php?p=admin">Retour à l'administration</a></p>
+<p><a href="index.php?p=admin" class="link_btn">Retour à l'administration</a></p>
 
 <h2>Ajouter un billet</h2>
 
@@ -14,7 +14,7 @@
         </div>
         <div class ="form-group">
             <label for="content">Contenu du billet :</label><br />
-            <textarea id="postContent" name="content" class="form-control" rows="20" cols="60"></textarea>
+            <textarea id="content" name="content" class="form-control" rows="20" cols="60"></textarea>
         </div>
         <div class ="form-group">
             <button type="submit" class="btn btn-primary" value="Ajouter" />Ajouter</button>
