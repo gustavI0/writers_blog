c30441953b6ee8eee80227a6808b2c51a8680baa
Jerome Guyon <gus.guyon@gmail.com>
2019-03-15 18:11:25 +0100

    Mise à jour de la mise en page du fichier

--- src/model/UserManager.php
+++ src/model/UserManager.php
@@ -6,20 +6,6 @@ require_once("model/Manager.php");
 
 class UserManager extends Manager {
 
-	public function inscription($pseudo, $pass_hache, $email) {
-
-		$db = $this->dbConnect();
-		$req = $db->prepare('
-			INSERT INTO users(pseudo, pwd, email) 
-			VALUES(:pseudo, :pwd, :email)');
-		$req->execute(array(
-			'pseudo' => $pseudo,
-			'pwd' => $pass_hache,
-			'email' => $email));
-
-		return $req;
-	}
-
 	public function getUserCred($pseudo) {
 
 		$db = $this->dbConnect();
@@ -32,6 +18,20 @@ class UserManager extends Manager {
 		$result = $req->fetch();
 		
 		return $result;
+	}
+
+	public function inscription($pseudo, $pass_hache, $email) {
+
+		$db = $this->dbConnect();
+		$req = $db->prepare('
+			INSERT INTO users(pseudo, pwd, email) 
+			VALUES(:pseudo, :pwd, :email)');
+		$req->execute(array(
+			'pseudo' => $pseudo,
+			'pwd' => $pass_hache,
+			'email' => $email));
+
+		return $req;
 	}
 
 }
