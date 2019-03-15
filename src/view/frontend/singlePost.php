0e7e293f25e70224b6a92175999564961f1dbc4b
Jerome Guyon <gus.guyon@gmail.com>
2019-03-15 18:18:07 +0100

    Mise à jour de la classe des liens pour leur donner une forme de bouton Gestion du texte HTML dans les billets Ajout d'un lien sur le titre vers la page d'accueil

--- src/view/frontend/singlePost.php
+++ src/view/frontend/singlePost.php
@@ -1,10 +1,12 @@
-<?php $title = $post['title']; ?>
+<?php 
+$blogTitle = 'Le blog de l\'écrivain';
+$title = $post['title']; ?>
 
 <?php ob_start(); ?>
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Le blog de l'écrivain</h1>
+            <h1><a href="index.php"><?= $blogTitle; ?></a></h1>
         </div>
     </div>
 </div>
@@ -12,7 +14,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <p><a href="index.php" class="link_btn">Retour à l'accueil</a></p>
+            <p><a href="index.php" class="btn btn_link">Retour à l'accueil</a></p>
         </div>
     </div>
 </div>
@@ -27,7 +29,7 @@
                 </div>
                 <div class="content_single_post">
                     <p>
-                        <?= nl2br(htmlspecialchars($post['content'])) ?>
+                        <?= $post['content'] ?>
                     </p>
                 </div>
             </div>
@@ -40,8 +42,7 @@
         <div class="col-sm-8 col-md-6">
             <h3>Commentaires</h3>
             <?php
-            while ($comment = $comments->fetch())
-            {
+            while ($comment = $comments->fetch()) {
             ?>
                 <div class="comment">
                     <p>
@@ -50,7 +51,7 @@
                     <p>
                         <?php
                             if ($comment['moderation'] == true) {
-                                echo $comment['comment'] = '<p><em> Commentaire modéré</em></p>';
+                                echo $comment['comment'] = '<p><em>Commentaire modéré !</em></p>';
                             } else {
                                 echo nl2br(htmlspecialchars($comment['comment']));
                             }
@@ -67,18 +68,20 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-8">
-            <form action="index.php?p=addComment&amp;id=<?= $post['id'] ?>" method="post">
-                <div class="form-group">
-                    <label for="author">Auteur</label><br />
-                    <input type="text" id="author" name="author" class="form-control" />
-                </div>
-                <div class="form-group">
-                    <label for="comment">Commentaire</label><br />
-                    <textarea id="comment" name="comment" class="form-control" rows="4" cols="50"></textarea>
-                </div>
-                    <button type="submit" class="btn btn-primary" value="Ajouter" />Ajouter</button>
-            </form>
+        <div class="col-sm-8 col-md-6">
+            <div class="add_comment">
+                <form action="index.php?p=addComment&amp;id=<?= $post['id'] ?>" method="post">
+                    <div class="form-group">
+                        <label for="author">Auteur</label><br />
+                        <input type="text" id="author" name="author" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label for="comment">Commentaire</label><br />
+                        <textarea id="comment" name="comment" class="form-control" rows="4" cols="50"></textarea>
+                    </div>
+                        <button type="submit" class="btn btn_basic" value="Ajouter" />Ajouter</button>
+                </form>
+            </div>
         </div>
     </div>
 </div>
