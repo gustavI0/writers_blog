53623d59463c461144c34ef0c33e4e5c402b744c
Jerome Guyon <gus.guyon@gmail.com>
2019-03-19 17:21:18 +0100

    Création d'une mini navbar dans le template

--- src/view/templates/admin.php
+++ src/view/templates/admin.php
@@ -6,28 +6,31 @@ include ('view/templates/head.php');
 ?>
         
     <body>
-        <nav>
-            <div>
-                <?php
-                if (isset($_SESSION['id']) AND isset($_SESSION['pseudo'])) {
-                    echo '<div class="dropdown">
-                    <a class="btn btn_link dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Bonjour ' . $_SESSION['pseudo'] . ' ! </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="index.php">Accès à la page d\'accueil</a>
-                    <a class="dropdown-item" href="index.php?p=disconnect">Se déconnecter</a>
-                    </div>
-                    </div>';
-                } else {
-                    echo '<a href="index.php?p=signin" class="btn btn_link">Se reconnecter</a>';
-                }
-                ?>
-            </div>
+        <nav class="col-sm-12">
+            <ul>
+                <li>
+                    <?php
+                    if (isset($_SESSION['id']) AND isset($_SESSION['pseudo'])) {
+                        echo '<div class="dropdown">
+                        <a class="btn btn_link dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Bonjour ' . $_SESSION['pseudo'] . ' ! </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="index.php">Accès à la page d\'accueil</a>
+                        <a class="dropdown-item" href="index.php?p=disconnect">Se déconnecter</a>
+                        </div>
+                        </div>';
+                    } else {
+                        echo '<a href="index.php?p=signin" class="btn btn_link">Se reconnecter</a>';
+                    }
+                    ?>
+                </li>
+                <li>
+                    <a href="index.php?p=admin" class="btn btn_link">Tableau de bord</a>
+                </li>
+            </ul>
         </nav>
         <main>
-            <div class="container">
-                <?= $content ?>
-            </div>
+            <?= $content ?>
         </main>
     </body>
 <?php include ('view/templates/footer.php'); ?>
--- src/view/templates/default.php
+++ src/view/templates/default.php
@@ -5,29 +5,32 @@ include ('view/templates/head.php');
 
 ?>
     <body>
-        <nav>
-            <div>
-                <?php
-                if (isset($_SESSION['id']) AND isset($_SESSION['pseudo'])) {
-                    echo '<div class="dropdown">
-                    <a class="btn btn_link dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Bonjour ' . $_SESSION['pseudo'] . ' ! 
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="index.php?p=admin">Accès au panneau d\'administration</a>
-                    <a class="dropdown-item" href="index.php?p=disconnect">Se déconnecter</a>
-                    </div>
-                    </div>';
-                } else {
-                    echo '<a href="index.php?p=signin" class="btn btn_link">Connexion</a>';
-                }
-                ?>
-            </div>
+        <nav class="col-sm-12">
+            <ul>
+                <li>
+                    <?php
+                    if (isset($_SESSION['id']) AND isset($_SESSION['pseudo'])) {
+                        echo '<div class="dropdown">
+                            <a class="btn btn_link dropdown-toggle" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Bonjour ' . $_SESSION['pseudo'] . ' ! 
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="index.php?p=admin">Accès au panneau d\'administration</a>
+                                <a class="dropdown-item" href="index.php?p=disconnect">Se déconnecter</a>
+                            </div>
+                        </div>';
+                    } else {
+                        echo '<a href="index.php?p=signin" class="btn btn_link">Connexion</a>';
+                    }
+                    ?>
+                </li>
+                <li>
+                    <a href="index.php" class="btn btn_link" id="return_home" >Accueil</a>
+                </li>
+            </ul>
         </nav>
     	<main>
-	    	<div class="container">
-	        	<?= $content ?>
-	    	</div>
+	        <?= $content ?>
     	</main>
     </body>
 <?php include ('view/templates/footer.php'); ?>
